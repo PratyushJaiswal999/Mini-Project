82c374bc34b5f6e296e1eaad3f4569108f0d5f50
PratyushJaiswal999 <pratyushjaiswal999@gmail.com>
2024-12-20 10:41:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,58 @@
-document.getElementById("contact-form").addEventListener("submit", function (e) {
-    e.preventDefault();
-    
-    const name = document.getElementById("name").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const message = document.getElementById("message").value.trim();
-    const formMessage = document.getElementById("form-message");
-  
-    if (name && email && message) {
-      formMessage.textContent = "Thank you for reaching out! We'll get back to you soon.";
-      formMessage.style.color = "green";
-      this.reset();
-    } else {
-      formMessage.textContent = "Please fill out all fields.";
-      formMessage.style.color = "red";
+const menuIcon = document.querySelector(".menu-icon");
+const xBtn = document.querySelector(".x-btn");
+const navigation = document.querySelector(".navigation");
+
+menuIcon.addEventListener("click", () => {
+  navigation.classList.add("navigate");
+});
+
+xBtn.addEventListener("click", () => {
+  navigation.classList.remove("navigate");
+});
+
+let start = false;
+
+window.addEventListener("scroll", () => {
+  const about = document.querySelector(".about");
+  const services = document.querySelector(".services");
+  const portfolio = document.querySelector(".portfolio");
+  const data = document.querySelector(".data");
+  const nums = document.querySelectorAll(".num");
+
+  if (window.pageYOffset >= 200) {
+    about.classList.add("change");
+  } else {
+    about.classList.remove("change");
+  }
+
+  if (window.pageYOffset >= about.offsetTop + 200) {
+    services.classList.add("change");
+  } else {
+    services.classList.remove("change");
+  }
+
+  if (window.pageYOffset >= services.offsetTop) {
+    portfolio.classList.add("change");
+  } else {
+    portfolio.classList.remove("change");
+  }
+
+  if (window.scrollY >= data.offsetTop - 300) {
+    if (!start) {
+      nums.forEach((num) => {
+        startCount(num);
+      });
     }
-  });
+    start = true;
+  }
+});
+
+const startCount = (el) => {
+  let max = el.dataset.val;
+  let count = setInterval(() => {
+    el.textContent++;
+    if (el.textContent === max) {
+      clearInterval(count);
+    }
+  }, 5);
+};
